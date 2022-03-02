20c4b131212e88e9c37048f1bd0bd9cd6ca56feb
StopHere1 <83949004+StopHere1@users.noreply.github.com>
2022-03-02 15:45:07 +0800

    updated function for locating the location of the two boxes and judge if the facing direction is right

--- main.py
+++ main.py
@@ -70,6 +70,49 @@ def calculate_focalDistance(img_path):
     return focalLength
 
 
+def is_lefthalf(box):
+    box= np.int0(box)
+    most_right = 0
+    most_left = 640
+
+    for k in range(0, 3):
+        if box[k][0] >= most_right:
+            most_right = box[k][0]
+        if box[k][0] <= most_left:
+            most_left = box[k][0]
+
+    if most_right < 320:  # 640/2
+        return 0
+    elif most_left > 320:
+        return 2
+    return 1
+
+
+def is_highhalf(box):
+    box = np.int0(box)
+    most_low = 480
+    most_high = 0
+
+    for k in range(0, 3):
+        if box[k][1] >= most_high:
+            most_low = box[k][1]
+        if box[k][1] <= most_low:
+            most_high = box[k][1]
+
+    if most_low < 240:  # 480/2
+        return 0
+    elif most_high > 240:
+        return 2
+    return 1
+
+
+def is_parallel(dist1, dist2):
+    if abs(dist1 - dist2) < 2:
+        return True
+    else:
+        return False
+
+
 img_path = "Picture1.jpg"
 focalLength = calculate_focalDistance(img_path)
 
@@ -88,7 +131,7 @@ while cap.isOpened():  # while the capture is open
             # delete backgrounds
             cnts = cv2.findContours(inRange_hsv.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[-2]
             # find contours
-
+            # print(frame.shape[0], ' ', frame.shape[1])  # 480 * 640 zero locates at
             # marker = find_marker(frame)
 
             # if len(cnts) > 1:
@@ -111,16 +154,34 @@ while cap.isOpened():  # while the capture is open
                 Max = max(cnts, key=cv2.contourArea)  # find outer edges of the rectangle
                 rect = cv2.minAreaRect(Max)  # draw the min area rectangle
                 box = cv2.boxPoints(rect)  # save the corner point to box
+
+                # if isinLeftHalf(box) == 0:
+                #     print('box1 in the left')
+                # elif isinLeftHalf(box) == 1:
+                #     print('box1 in the middle')
+                # elif isinLeftHalf(box) == 2:
+                #     print('box1 in the right')
+                #
+                # if isinHighHalf(box) == 0:
+                #     print('box1 in the high')
+                # elif isinHighHalf(box) == 1:
+                #     print('box1 in the middle')
+                # elif isinHighHalf(box) == 2:
+                #     print('box1 in the low')
+
                 if rect[1][0] != 0:
                     if rect[1][1] < rect[1][0]:
                         inches = distance_to_camera(KNOWN_WIDTH, focalLength, rect[1][0])
-                        cv2.putText(frame, "%.2fcm" % (inches * 2.54), (frame.shape[1] - 200, frame.shape[0] - 20),
-                                    cv2.FONT_HERSHEY_SIMPLEX, 2.0, (0, 255, 0), 3)
+                        distance1 = inches * 2.4
+                        # cv2.putText(frame, "%.2fcm" % (inches * 2.54), (frame.shape[1] - 200, frame.shape[0] - 20),
+                        #             cv2.FONT_HERSHEY_SIMPLEX, 2.0, (0, 255, 0), 3)
                     else:
                         inches = distance_to_camera(KNOWN_WIDTH, focalLength, rect[1][1])
-                        cv2.putText(frame, "%.2fcm" % (inches * 2.54), (frame.shape[1] - 200, frame.shape[0] - 20),
-                                    cv2.FONT_HERSHEY_SIMPLEX, 2.0, (0, 255, 0), 3)
+                        distance1 = inches * 2.4
+                        # cv2.putText(frame, "%.2fcm" % (inches * 2.54), (frame.shape[1] - 200, frame.shape[0] - 20),
+                        #             cv2.FONT_HERSHEY_SIMPLEX, 2.0, (0, 255, 0), 3)
 
+                print('distance1 = ', distance1)
                 cv2.drawContours(frame, [np.int0(box)], -1, (0, 255, 255), 2)
                 if len(cnts) > 1:
                     temp = cnts
@@ -134,21 +195,38 @@ while cap.isOpened():  # while the capture is open
                     # print("Max")
                     rect2 = cv2.minAreaRect(secondMax)  # draw the min area rectangle
                     box2 = cv2.boxPoints(rect2)  # save the corner point to box
-                    print('box2 = ', box2)
+                    # print('box2 = ', box2)
+
+                    # if isinLeftHalf(box2) == 0:
+                    #     print('box2 in the left')
+                    # elif isinLeftHalf(box2) == 1:
+                    #     print('box2 in the middle')
+                    # elif isinLeftHalf(box2) == 2:
+                    #     print('box2 in the right')
+                    #
+                    # if isinHighHalf(box2) == 0:
+                    #     print('box2 in the high')
+                    # elif isinHighHalf(box2) == 1:
+                    #     print('box2 in the middle')
+                    # elif isinHighHalf(box2) == 2:
+                    #     print('box2 in the low')
 
                     if rect2[1][0] != 0:
                         if rect2[1][1] < rect2[1][0]:
                             inches = distance_to_camera(KNOWN_WIDTH, focalLength, rect2[1][0])
+                            distance2 = inches * 2.4
                             # print('rect2[1][0] = ', rect2[1][0])
-                            cv2.putText(frame, "%.2fcm" % (inches * 2.54),
-                                        (frame.shape[1] - 400, frame.shape[0] - 40),
-                                        cv2.FONT_HERSHEY_SIMPLEX, 2.0, (0, 255, 0), 3)
+                            # cv2.putText(frame, "%.2fcm" % (inches * 2.54),
+                            #             (frame.shape[1] - 400, frame.shape[0] - 40),
+                            #             cv2.FONT_HERSHEY_SIMPLEX, 2.0, (0, 255, 0), 3)
                         else:
                             inches = distance_to_camera(KNOWN_WIDTH, focalLength, rect2[1][1])
+                            distance2 = inches * 2.4
                             # print('rect2[1][0] = ', rect2[1][0])
-                            cv2.putText(frame, "%.2fcm" % (inches * 2.54),
-                                        (frame.shape[1] - 400, frame.shape[0] - 40),
-                                        cv2.FONT_HERSHEY_SIMPLEX, 2.0, (0, 255, 0), 3)
+                            # cv2.putText(frame, "%.2fcm" % (inches * 2.54),
+                            #             (frame.shape[1] - 400, frame.shape[0] - 40),
+                            #             cv2.FONT_HERSHEY_SIMPLEX, 2.0, (0, 255, 0), 3)
+                    print('distance2 = ', distance2)
                     cv2.drawContours(frame, [np.int0(box2)], -1, (255, 255, 255), 2)
                     # counter = counter + 1
 
