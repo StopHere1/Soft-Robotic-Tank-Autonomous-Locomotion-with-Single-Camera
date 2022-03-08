562d736f16bf95cb5229634ed1dcaffad169ac52
StopHere1 <83949004+StopHere1@users.noreply.github.com>
2022-03-09 00:16:34 +0800

    just some small changes

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ import cv2
 import serial.tools.list_ports
 import time
 
-
 import imutils
 import matplotlib.pyplot as plt
 
@@ -35,7 +34,7 @@ KNOWN_WIDTH = 0.4724
 KNOWN_HEIGHT = 1.5748
 
 
-def find_marker(image):
+def find_marker(image):  # function to find color block
     gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     gray_img = cv2.GaussianBlur(gray_img, (5, 5), 0)
     edged_img = cv2.Canny(gray_img, 35, 125)
@@ -47,7 +46,8 @@ def find_marker(image):
 
 
 # IMAGE_PATH = ["Picture1.jpg", "Picture2.jpg"]
-focalLength = 2.8
+
+# focalLength = 2.8
 
 
 # image = cv2.imread(IMAGE_PATH[0])
@@ -55,14 +55,14 @@ focalLength = 2.8
 # focalLength = (marker[1][0] * KNOWN_DISTANCE) / KNOWN_WIDTH
 
 
-def distance_to_camera(knownWidth, focalLength, perWidth):
+def distance_to_camera(knownWidth, focalLength, perWidth):  # function to get the distance to the camera(inch)
     # if perWidth != 0:
     return (knownWidth * focalLength) / perWidth
 
 
 # return 0
 
-def calculate_focalDistance(img_path):
+def calculate_focalDistance(img_path):  # function to calculate focaldistance using image saved
     first_image = cv2.imread(img_path)
 
     marker = find_marker(first_image)
@@ -74,8 +74,8 @@ def calculate_focalDistance(img_path):
     return focalLength
 
 
-def is_lefthalf(box):
-    box= np.int0(box)
+def is_lefthalf(box):  # determine if the color block is in the left half
+    box = np.int0(box)
     most_right = 0
     most_left = 640
 
@@ -92,7 +92,7 @@ def is_lefthalf(box):
     return 1
 
 
-def is_highhalf(box):
+def is_highhalf(box):  # determine if the color block is in the higher half
     box = np.int0(box)
     most_low = 480
     most_high = 0
@@ -110,15 +110,32 @@ def is_highhalf(box):
     return 1
 
 
-def is_parallel(dist1, dist2):
+def is_parallel(dist1, dist2):  # determine if the color blocks are parallel
     if abs(dist1 - dist2) < 2:
         return True
     else:
         return False
 
 
-img_path = "Picture1.jpg"
+# function to get the distance between the middle point of color blocks and the center of the picture
+def calculate_error(box1, box2):
+    pass
+
+
+img_path = "Picture1.jpg"  # image saved for focalDistance calculation
 focalLength = calculate_focalDistance(img_path)
+
+plist = list(serial.tools.list_ports.comports())
+
+# set up serials and COM
+if len(plist) <= 0:
+    print("no serial")
+else:
+    plist_0 = list(plist[0])
+    print(plist_0)
+    serialName = 'COM5'  # plist_0[0] choose according to arduino ide
+    serialFd = serial.Serial(serialName, 9600, timeout=60)
+    print("serial name ", serialFd.name)
 
 #
 while cap.isOpened():  # while the capture is open
@@ -156,8 +173,8 @@ while cap.isOpened():  # while the capture is open
                 # cnts.sort(key=cv2.contourArea(cnts), reverse=False)
 
                 Max = max(cnts, key=cv2.contourArea)  # find outer edges of the rectangle
-                rect = cv2.minAreaRect(Max)  # draw the min area rectangle
-                box = cv2.boxPoints(rect)  # save the corner point to box
+                rect1 = cv2.minAreaRect(Max)  # draw the min area rectangle
+                box1 = cv2.boxPoints(rect1)  # save the corner point to box
 
                 # if isinLeftHalf(box) == 0:
                 #     print('box1 in the left')
@@ -173,20 +190,20 @@ while cap.isOpened():  # while the capture is open
                 # elif isinHighHalf(box) == 2:
                 #     print('box1 in the low')
 
-                if rect[1][0] != 0:
-                    if rect[1][1] < rect[1][0]:
-                        inches = distance_to_camera(KNOWN_WIDTH, focalLength, rect[1][0])
+                if rect1[1][0] != 0:
+                    if rect1[1][1] < rect1[1][0]:
+                        inches = distance_to_camera(KNOWN_WIDTH, focalLength, rect1[1][0])
                         distance1 = inches * 2.4
                         # cv2.putText(frame, "%.2fcm" % (inches * 2.54), (frame.shape[1] - 200, frame.shape[0] - 20),
                         #             cv2.FONT_HERSHEY_SIMPLEX, 2.0, (0, 255, 0), 3)
                     else:
-                        inches = distance_to_camera(KNOWN_WIDTH, focalLength, rect[1][1])
+                        inches = distance_to_camera(KNOWN_WIDTH, focalLength, rect1[1][1])
                         distance1 = inches * 2.4
                         # cv2.putText(frame, "%.2fcm" % (inches * 2.54), (frame.shape[1] - 200, frame.shape[0] - 20),
                         #             cv2.FONT_HERSHEY_SIMPLEX, 2.0, (0, 255, 0), 3)
 
                 print('distance1 = ', distance1)
-                cv2.drawContours(frame, [np.int0(box)], -1, (0, 255, 255), 2)
+                cv2.drawContours(frame, [np.int0(box1)], -1, (0, 255, 255), 2)
                 if len(cnts) > 1:
                     temp = cnts
                     secondMax = cnts[0]
@@ -240,6 +257,13 @@ while cap.isOpened():  # while the capture is open
 
             cv2.imshow('camera', frame)  # show the frame
             cv2.waitKey(1)  # let the frame wait
+
+            # messages transmitting to arduino
+            serialFd.write("w".encode())
+            # confirming the work done
+            while serialFd.readLine() is None:
+                continue
+            print(serialFd.readLine())
         else:
             print("No picture")
     else:
