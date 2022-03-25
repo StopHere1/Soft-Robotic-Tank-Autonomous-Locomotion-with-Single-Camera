e07d05cb9077c0da9517dbebef5f38d9240bfc12
StopHere1 <83949004+StopHere1@users.noreply.github.com>
2022-03-25 14:38:41 +0800

    updated 20200325

--- softrobot/softrobot.ino
+++ softrobot/softrobot.ino
@@ -15,7 +15,8 @@ int Pin9 = 9;
 int Pin10 = 10;
 int Pin11 = 11;
 int Pin12 = 12;
-
+int Pin13 = 13;
+char Lastchar = "";
 void goAhead(int time); //forward
 void pump(boolean flag); //pumps controlling
 void turnRight(int time1); //turn right
@@ -26,8 +27,8 @@ void keep(int x); //keep current pressure
 
 void setup()
 {
-  Serial.begin(9600);
-  Serial.setTimeout(0.5);
+  Serial.begin(115200);
+  Serial.setTimeout(0.1);
   pinMode(Pin1, OUTPUT);
   pinMode(Pin2, OUTPUT);
   pinMode(Pin3, OUTPUT);
@@ -40,160 +41,183 @@ void setup()
   pinMode(Pin10, OUTPUT);
   pinMode(Pin11, OUTPUT);
   pinMode(Pin12, OUTPUT);
-//  middle(false);
+  pinMode(Pin13, OUTPUT);
+  pump(true);
+  middle(true);
 }
 
 void loop()
 {
   char Command = Serial.read();
-  
   switch (Command) {
     case 'w' :
       goAhead(1000);
+      Lastchar =  'w';
       break;
     case 'a':
       turnLeft(1000);
+      Lastchar =  'a';
       break;
     case 'd':
       turnRight(1000);
+      Lastchar =  'd';
       break;
     case 'q':
       pump(true);
+      Lastchar =  'q';
       break;
     case 'e':
       pump(false);
+      Lastchar =  'e';
       break;
     case 'g':
       middle(true);
+      Lastchar =  'g';
       break;
     case 'h':
       middle(false);
+      Lastchar =  'h';
       break;
     default:
-      break;
+      switch (Lastchar) {
+        case 'w' :
+          goAhead(1000);
+          break;
+        case 'a':
+          turnLeft(1000);
+          break;
+        case 'd':
+          turnRight(1000);
+          break;
+        case 'q':
+          pump(true);
+          break;
+        case 'e':
+          pump(false);
+          break;
+        case 'g':
+          middle(true);
+          break;
+        case 'h':
+          middle(false);
+          break;
+        default:
+          break;
+      }
   }
 }
-void goAhead(int time1) {
+  void goAhead(int time1) {
 
-  digitalWrite(Pin3, HIGH); //right front
-  digitalWrite(Pin4, LOW); //right front
-  digitalWrite(Pin5, LOW); //right back
-  digitalWrite(Pin6, HIGH); //right back
-  digitalWrite(Pin7, HIGH); //left front
-  digitalWrite(Pin8, LOW); //left front
-  digitalWrite(Pin9, LOW); //left back
-  digitalWrite(Pin10, HIGH); //left back
-  delay(time1);
+    digitalWrite(Pin3, HIGH); //right front
+    digitalWrite(Pin4, LOW); //right front
+    digitalWrite(Pin5, LOW); //right back
+    digitalWrite(Pin6, HIGH); //right back
+    digitalWrite(Pin7, HIGH); //left front
+    digitalWrite(Pin8, LOW); //left front
+    digitalWrite(Pin9, LOW); //left back
+    digitalWrite(Pin10, HIGH); //left back
+    delay(time1);
 
-  digitalWrite(Pin3, LOW); //right front
-  digitalWrite(Pin4, HIGH); //right front
-  digitalWrite(Pin5, HIGH); //right back
-  digitalWrite(Pin6, LOW); //right back
-  digitalWrite(Pin7, LOW); //left front
-  digitalWrite(Pin8, HIGH); //left front
-  digitalWrite(Pin9, HIGH); //left back
-  digitalWrite(Pin10, LOW); //left back
-  delay(time1);
-  keep(1);
-  keep(2);
+    digitalWrite(Pin3, LOW); //right front
+    digitalWrite(Pin4, HIGH); //right front
+    digitalWrite(Pin5, HIGH); //right back
+    digitalWrite(Pin6, LOW); //right back
+    digitalWrite(Pin7, LOW); //left front
+    digitalWrite(Pin8, HIGH); //left front
+    digitalWrite(Pin9, HIGH); //left back
+    digitalWrite(Pin10, LOW); //left back
+    delay(time1);
+    keep(1);
+    keep(2);
 
-  Serial.print(1);
-}
-
-void turnRight(int time1) {
-
-  digitalWrite(Pin7, HIGH); //right front
-  digitalWrite(Pin8, LOW); //right front
-  digitalWrite(Pin9, LOW); //right back
-  digitalWrite(Pin10, HIGH); //right back
-
-  delay(time1);
-
-  digitalWrite(Pin7, LOW); //right front
-  digitalWrite(Pin8, HIGH); //right front
-  digitalWrite(Pin9, HIGH); //right back
-  digitalWrite(Pin10, LOW); //right back
-
-  delay(time1);
-
-  keep(2);
-  Serial.print(1);
-}
-
-void turnLeft(int time1) {
-
-  digitalWrite(Pin3, HIGH); //right front
-  digitalWrite(Pin4, LOW); //right front
-  digitalWrite(Pin5, LOW); //right back
-  digitalWrite(Pin6, HIGH); //right back
-
-  delay(time1);
-
-  digitalWrite(Pin3, LOW); //right front
-  digitalWrite(Pin4, HIGH); //right front
-  digitalWrite(Pin5, HIGH); //right back
-  digitalWrite(Pin6, LOW); //right back
-
-  delay(time1);
-  keep(1);
-  Serial.print(1);
-}
-
-void pump(boolean flag) {
-  if (flag) {
-//    digitalWrite(Pin1, HIGH);
-    digitalWrite(Pin2, HIGH);
+    Serial.print(1);
   }
-  else {
-//    digitalWrite(Pin1, LOW);
-    digitalWrite(Pin2, LOW);
-  }
-  Serial.print(1);
-}
 
-void middle(boolean flag) {
-  if (flag) {
+  void turnRight(int time1) {
+
+    digitalWrite(Pin7, HIGH); //right front
+    digitalWrite(Pin8, LOW); //right front
+    digitalWrite(Pin9, LOW); //right back
+    digitalWrite(Pin10, HIGH); //right back
+
+    delay(time1 );
+
+    digitalWrite(Pin7, LOW); //right front
+    digitalWrite(Pin8, HIGH); //right front
+    digitalWrite(Pin9, HIGH); //right back
+    digitalWrite(Pin10, LOW); //right back
+
+    delay(time1 );
+
+    keep(2);
+    Serial.print(1);
+  }
+
+  void turnLeft(int time1) {
+
+    digitalWrite(Pin3, HIGH); //right front
+    digitalWrite(Pin4, LOW); //right front
+    digitalWrite(Pin5, LOW); //right back
+    digitalWrite(Pin6, HIGH); //right back
+
+    delay(time1 );
+
+    digitalWrite(Pin3, LOW); //right front
+    digitalWrite(Pin4, HIGH); //right front
+    digitalWrite(Pin5, HIGH); //right back
+    digitalWrite(Pin6, LOW); //right back
+
+    delay(time1 );
+    keep(1);
+    Serial.print(1);
+  }
+
+  void pump(boolean flag) {
+    if (flag) {
+      //    digitalWrite(Pin1, HIGH);
+      digitalWrite(Pin2, HIGH);
+      digitalWrite(Pin13, HIGH);
+    }
+    else {
+      //    digitalWrite(Pin1, LOW);
+      digitalWrite(Pin2, LOW);
+      digitalWrite(Pin13, LOW);
+    }
+    Serial.print(1);
+  }
+
+  void middle(boolean flag) {
     digitalWrite(Pin11, HIGH);
     digitalWrite(Pin12, LOW);
-    delay(4000);
-    digitalWrite(Pin11, LOW);
-    digitalWrite(Pin12, LOW);
+    Serial.print(1);
   }
-  else {
-    digitalWrite(Pin11, LOW);
-    digitalWrite(Pin12, HIGH);
-    delay(4500);
-    digitalWrite(Pin11, LOW);
-    digitalWrite(Pin12, LOW);
-  }
-  keep(3);
-  Serial.print(1);
-}
 
-void reset() {
-  for (int i = 1; i <= 12; i++) {
-    if (digitalRead(i) == HIGH) {
-      digitalWrite(i, LOW);
+  void reset() {
+    for (int i = 1; i <= 12; i++) {
+      if (digitalRead(i) == HIGH) {
+        digitalWrite(i, LOW);
+      }
+    }
+    delay(1000);
+    Serial.print(1);
+  }
+
+  void keep(int x) { // function to keep the presure
+    switch (x) {
+      case 1:
+        digitalWrite(Pin3, LOW); //right front
+        digitalWrite(Pin4, LOW); //right front
+        digitalWrite(Pin5, LOW); //right back
+        digitalWrite(Pin6, LOW); //right back
+        break;
+      case 2:
+        digitalWrite(Pin7, LOW); //right front
+        digitalWrite(Pin8, LOW); //right front
+        digitalWrite(Pin9, LOW); //right back
+        digitalWrite(Pin10, LOW); //right back
+        break;
+      case 3:
+        digitalWrite(Pin11, LOW); //right front
+        digitalWrite(Pin12, LOW); //right front
     }
   }
-  delay(1000);
-  Serial.print(1);
-}
-
-void keep(int x) {
-  switch (x) {
-    case 1:
-      digitalWrite(Pin3, LOW); //right front
-      digitalWrite(Pin4, LOW); //right front
-      digitalWrite(Pin5, LOW); //right back
-      digitalWrite(Pin6, LOW); //right back
-    case 2:
-      digitalWrite(Pin7, LOW); //right front
-      digitalWrite(Pin8, LOW); //right front
-      digitalWrite(Pin9, LOW); //right back
-      digitalWrite(Pin10, LOW); //right back
-    case 3:
-      digitalWrite(Pin11, LOW); //right front
-      digitalWrite(Pin12, LOW); //right front
-  }
-}
